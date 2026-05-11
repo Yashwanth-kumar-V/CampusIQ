2bbb243a38c4c32232c934057b9a27c84d57b54c
Yashwanth-kumar-V <yashwanthkumar.v.2024.cse@rajalakshmi.edu.in>
2026-05-12 00:28:52 +0530

    Backend server update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = window.location.hostname === 'localhost'
   ? "http://localhost:5000"
-  : "https://campusiq.venkatengineer.site/";
+  : "https://campusiq.venkatengineer.site";
 
 export default API_BASE_URL;
